e19172219f6c2dc1f1e66e34a4535a697f729f3e
joshvilson <jv265@njit.edu>
2020-10-24 14:26:26 -0400

    Feature: Added multiplication to calculator

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,9 +1,18 @@
 def addition(a, b):
+    a = int(a)
+    b = int(b)
     return a + b
 
 
 def subtraction(a, b):
-    return a - b
+    a = int(a)
+    b = int(b)
+    return b - a
+
+def multiplication(a, b):
+    a = int(a)
+    b = int(b)
+    return a * b
 
 
 class Calculator:
@@ -18,4 +27,8 @@ class Calculator:
 
     def subtract(self, a, b):
         self.result = subtraction(a, b)
+        return self.result
+
+    def multiplication(self, a, b):
+        self.result = multiplication(a, b)
         return self.result
